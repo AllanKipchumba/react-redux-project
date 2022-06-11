2ee4053db7e040d73c73a85c79269d7497cdb7ce
_Allan <allankipchumba76@gmail.com>
2022-06-11 23:55:50 +0300

    created components, installed a few dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Counter from "./components/Counter";
+import Employees from "./components/Employees";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import UserList from "./components/UserList";
 
-function App() {
+const App = () => {
   return (
     <>
-      <div>Hello from react</div>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/users" element={<UserList />} />
+        </Routes>
+      </div>
     </>
   );
-}
+};
 
 export default App;
