2a50820d3b92154175d5c387531022e33148327a
_Allan <allankipchumba76@gmail.com>
2022-06-12 13:15:10 +0300

    partially completed navBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,56 @@
 import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import styles from "../css/style.module.css";
+import "../css/styles.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [nav, setNav] = useState(true);
-
   const handleNav = () => setNav(!nav);
 
+  const [showMenuItems, setShowMenuItems] = useState(true);
+  const handleClick = () => setShowMenuItems(!showMenuItems);
+
   return (
-    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a] uppercase m-4 bg-[#000300]">
-        React.
-      </h1>
-      <ul className="hidden md:flex">
-        <li className="p-4">Home</li>
-        <li className="p-4">Company</li>
-        <li className="p-4">Resources</li>
-        <li className="p-4">About</li>
-        <li className="p-4">Contact</li>
-      </ul>
-
-      <div onClick={handleNav} className="block md:hidden">
-        {!nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
-      </div>
-
-      <div
-        className={
-          !nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
-            : "fixed left-[-100%]"
-        }
-      >
-        <h1 className="w-full text-3xl font-bold text-[#00df9a] uppercase m-4">
-          React.
-        </h1>{" "}
-        <ul className="uppercase p-4">
-          <li className="p-4 border-b border-gray-600">Home</li>
-          <li className="p-4 border-b border-gray-600">Company</li>
-          <li className="p-4 border-b border-gray-600">Resources</li>
-          <li className="p-4 border-b border-gray-600">About</li>
-          <li className="p-4">Contact</li>
+    <>
+      <nav className={styles.nav}>
+        <h1 className={styles.h1}>Redux Toolkit</h1>
+        <ul className="hidden md:flex ulitems">
+          <li>
+            <Link to={"/employees"}>Employees</Link>
+          </li>
+          <li>
+            <Link to={"/counter"}>Counter</Link>
+          </li>
+          <li>
+            <Link to={"/users"}>Users</Link>
+          </li>
         </ul>
-      </div>
-    </div>
+
+        {/* Navigigation menu */}
+        <div onClick={handleNav} className="block md:hidden">
+          {!nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
+        </div>
+
+        {/* <div className={!showMenuItems ? "hidden" : null}> */}
+        <div className={!nav ? styles.showNav : styles.hideNav}>
+          <h1 className={styles.h1}>Redux Toolkit</h1>
+
+          <ul className={styles.navItems}>
+            <li onClick={handleClick}>
+              <Link to={"/employees"}>Employees</Link>
+            </li>
+            <li onClick={handleClick}>
+              <Link to={"/counter"}>Counter</Link>
+            </li>
+            <li onClick={handleClick}>
+              <Link to={"/users"}>Users</Link>
+            </li>
+          </ul>
+        </div>
+        {/* </div> */}
+      </nav>
+    </>
   );
 };
 
